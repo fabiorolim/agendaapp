feb116f6e1446478496d9005b95cf7dc31ea2627
fabiorolim <fabio_rolim@hotmail.com>
2018-11-06 16:47:15 -0300

    Genesis

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, List } from 'ionic-angular';
+import { Contato } from '../../models/Contato';
+import { HttpClient } from '@angular/common/http'
+import { Carro } from '../../models/Carro';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public contatos: Contato[];
 
+  constructor(public navCtrl: NavController, private _http: HttpClient) {
+    
+    this._http.get<Contato[]>('http://localhost:5000/api')
+        .subscribe(
+          (contatos) => {
+            console.log(contatos);
+            this.contatos = contatos
+          }
+        );
+        console.log(this.contatos);
   }
-
 }
